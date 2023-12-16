26d7078803dd34b54c37e8c90aed2daf46b105d8
Xiraeth <77112995+Xiraeth@users.noreply.github.com>
2023-12-16 16:02:12 +0200

    Initial commit

---
+++ odin-cv/README.md
@@ -0,0 +1,3 @@
+# The Odin Project - CV Application
+
+CV application on "The Odin Project" built with React
---
+++ odin-cv/src/components/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "../style.css";
+
+function App() {
+  return (
+    <section className="leftSide">
+      <div className="personalInfoContainer">
+        <h1>Testing</h1>
+      </div>
+    </section>
+  );
+}
+
+export default App;
